5ae56e89f9b217930c579f3f6af70969b448ef6c
Nisidh Kumar <kumarnisidh@gmail.com>
2025-11-10 09:58:28 +0530

    Update variable names to uppercase in greeting

--- jenkinsPractice.sh
+++ jenkinsPractice.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "Enter your name:"
-read name
-echo "What's your favorite programming language?"
-read language
-echo "Hello, $name! You love coding in $language. Keep it up!"
+echo "Hello, $NAME! You love coding in $LANGUAGE. Keep it up!"
